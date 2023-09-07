99c67f45c9645a639f2034f4ee0334fd1ef912f2
MariaGKimani <mariakimanigoretti@gmail.com>
2023-09-07 11:35:01 +0300

    modified app.test file

--- nexus-informational/src/App.test.js
+++ nexus-informational/src/App.test.js
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders a part of the Product component in the App component', () => {
   render(<App />);
-  const productElement = screen.getByText(/What We Do/i); // Replace with actual text from Product component
+  const productElement = screen.getByText(/What We Do/i);
   expect(productElement).toBeInTheDocument();
 });
