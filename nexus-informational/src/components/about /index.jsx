7db7af40ea651bad50c51526973e2c8bed4111f9
Rachel Tuyishimire <rachelltuyishimire@gmail.com>
2023-09-08 00:41:45 +0300

    Adjusted alignment

--- nexus-informational/src/components/about /index.jsx
+++ nexus-informational/src/components/about /index.jsx
@@ -3,21 +3,19 @@ import './style.css'
 
 const About = ()=> {
   return (
-    <div className='body'>
+    <div>
       <div className='body'>
           <h2 className='title'>Why Nexus?</h2>
     <div className='about'>
-      
+      <div>
       <p className='text' data-testid= "text">
-      Welcome to the future of energy management with the <strong>Nexus Interface Unit.</strong>
+      Welcome to the future of energy management with the Nexus Interface Unit.
       This innovative device offers real-time power monitoring,providing you with constant updates on your power capacity 
       and insights to enhance your energy management.It introduces a groundbreaking power-sharing feature, addressing situations 
       where electricity resources are limited and enabling seamless collaboration with neighboring users. 
-      
       Our mission is to empower hydro mini-grid users in rural Kenya, revolutionizing the way they plan and monitor their power consumption. 
-      
-
       </p>
+      </div>
       <div className='shapes'>
       <div className="square1" data-testid = "square1"></div><br/>
       <div className="square2" data-testid = "square2"></div><br/>
