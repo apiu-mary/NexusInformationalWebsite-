6d9b30fc43b7d5fd5726997e4e674493bf96f891
Moraaalice <alicemoraaongongo@gmail.com>
2023-09-07 23:00:15 +0300

    Created the home pages of nexus

---
+++ nexus-informational/src/Components/home/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import './style.css'
+const Home = () =>{
+    return(
+        <div className="home">
+            <img className="img" alt="Mini-grids" src="/img/images/minigrid.png"/>
+        <h1>Realtime Monitoring Of Power</h1>
+        </div>
+    );
+};
+export default Home;
