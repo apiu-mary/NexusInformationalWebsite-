63392fbafa4ff41eb852c2d882c058424a99163c
njorogewambuielizabeth <njorogewambuielizabeth@gmail.com>
2023-09-13 11:46:48 +0300

    commented out the workflows code

--- nexus-team/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders the App component', () => {
-  render(<App />);
-  const appElement = screen.getByTestId('app'); 
-  expect(appElement).toBeInTheDocument();
-});
