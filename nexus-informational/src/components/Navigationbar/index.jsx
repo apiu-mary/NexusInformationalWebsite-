be098c45212ff770d637c482019fe67cd0d7eea2
apiu-mary <apiuuagou@gmail.com>
2023-09-14 20:35:04 +0300

    added styles

--- nexus-informational/src/components/Navigationbar/index.jsx
+++ nexus-informational/src/components/Navigationbar/index.jsx
@@ -1,53 +1,87 @@
 
- import React, { useState } from 'react';
+//  import React, { useState } from 'react';
  
- import './style.css';
+//  import './style.css';
 
-function Navbar() {
-  const [isNavOpen, setNavOpen] = useState(false);
+// function Navbar() {
+//   const [isNavOpen, setNavOpen] = useState(false);
+//   const toggleNav = () => {
+//     setNavOpen(!isNavOpen);
+//   };
+//   return (
+//     <nav>
+//       <div className={`navbar-wrapper ${isNavOpen ? 'nav-open' : ''}`}>
+//         <span className="brand-container">
+//           <a className="logo-link" href="#logo">
+//             <img src={ 'img/images/Logo.png'} alt="Logo"className="logo-img"/>
+//           </a>
+//         </span>
+//         <div
+//           data-testid="hamburger-menu"
+//           className={`hamburger-menu ${isNavOpen ? 'open' : ''}`}
+//           onClick={toggleNav}
+//         >
+//           {isNavOpen ? (
+//             <div className="close-icon">
+//               <span></span>
+//               <span></span>
+//             </div>
+//           ) : (
+//             <>
+//               <div className="bar"></div>
+//               <div className="bar"></div>
+//               <div className="bar"></div>
+//             </>
+//           )}
+//         </div>
+//         <ul data-testid="nav-list" className={`nav-list ${isNavOpen ? 'open' : ''}`}>
+//           <div className="centered-links">
+//             <li><a href="Home">Home</a></li>
+//             <li><a href="#About Us">About</a></li>
+//             <li><a href="#What We do">Product</a></li>
+//             <li><a href="#footer">Contact</a>
+//             </li>
+//           </div>
+//         </ul>
+//       </div>
+//     </nav>
+//   );
+//           }
+
+
+// export default Navbar;
+
+
+import React, { useState } from "react";
+import "./style.css";
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
   const toggleNav = () => {
-    setNavOpen(!isNavOpen);
+    setIsOpen(!isOpen);
   };
   return (
-    <nav>
-      <div className={`navbar-wrapper ${isNavOpen ? 'nav-open' : ''}`}>
-        <span className="brand-container">
-          <a className="logo-link" href="#logo">
-            <img src={ 'img/images/Logo.png'} alt="Logo"className="logo-img"/>
-          </a>
-        </span>
-        <div
-          data-testid="hamburger-menu"
-          className={`hamburger-menu ${isNavOpen ? 'open' : ''}`}
-          onClick={toggleNav}
-        >
-          {isNavOpen ? (
-            <div className="close-icon">
-              <span></span>
-              <span></span>
-            </div>
-          ) : (
-            <>
-              <div className="bar"></div>
-              <div className="bar"></div>
-              <div className="bar"></div>
-            </>
-          )}
-        </div>
-        <ul data-testid="nav-list" className={`nav-list ${isNavOpen ? 'open' : ''}`}>
-          <div className="centered-links">
-            <li><a href="Home">Home</a></li>
-            <li><a href="#About Us">About</a></li>
-            <li><a href="#What We do">Product</a></li>
-            <li><a href="#footer">Contact</a>
-            </li>
-          </div>
-        </ul>
+    <div className="Navbar">
+      <span className="logo">
+        <img src="./img/images/Logo.png" alt="" />
+      </span>
+      <div className={`nav-items ${isOpen && "open"}`} data-testid="nav-items">
+        <a href="/our services">Home</a>
+        <a href="/our team">About Us</a>
+        <a href="/our team">What We Do</a>
+        <a href="/our team">Our Team</a>
+        <a href="/contact us">Contact Us</a>
+       
       </div>
-    </nav>
+      <div
+        className={`nav-toggle ${isOpen && "open"}`}
+        onClick={toggleNav}
+        data-testid="nav-toggle"
+      >
+        <div className="bar"></div>
+        {isOpen && <div className="overlay" onClick={toggleNav}></div>}
+      </div>
+    </div>
   );
-          }
-
-
+};
 export default Navbar;
 
--- nexus-informational/src/components/home/index.js
+++ nexus-informational/src/components/home/index.js
@@ -1,14 +1,33 @@
-import React from "react";
-import './style.css'
+import React, { useEffect, useState } from "react";
+import './style.css';
 
+const Home = () => {
+  // State to keep track of the screen width
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
-const Home = () =>{
-    return(
-   <div className="home">
-        <div className="background">
+  // Function to update windowWidth when the window is resized
+  const handleResize = () => {
+    setWindowWidth(window.innerWidth);
+  };
+
+  // Add event listener to window resize
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  // Determine whether to hide the background based on screen width
+  const shouldHideBackground = windowWidth <= 700;
+
+  return (
+    <div className={`home ${shouldHideBackground ? 'hide-background' : ''}`}>
+      <div className="background">
         <h1>Realtime Monitoring Of Power</h1>
-        </div>
-   </div>
-    );
+      </div>
+    </div>
+  );
 };
+
 export default Home;
